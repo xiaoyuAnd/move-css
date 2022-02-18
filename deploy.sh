8b7b46465f031c0973cef0aebd24a1092f13a37a
Frank <2521647197@qq.com>
2022-02-18 20:02:24 +0800

    1-2

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
+#!/usr/bin/env sh
+
+# 终止一个错误
+set -e
+
 # 构建
-rm -rf dist && yarn build src/index.html --public-url ./ --no-cache --no-minify
-# rm -rf dist && rm -rf .cache
+rm -rf dist && parcel build src/index.html --no-cache --no-minify
+
 # 进入生成的构建文件夹
 cd dist
 
-# 初始化 git
+# 如果你是要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 部署到 GitHub Pages
-git push -f git@github.com:xiaoyuAnd/move-css.git master:gh-pages
 git push -f git@github.com:xiaoyuAnd/move-css.git master:gh-pages
 
 cd -
